481c30adec8dce465c2e5e7531353d40488c4cc4
zhao-li- <1216186168@qq.com>
2014-05-17 16:11:30 +0800

    $location

--- app/scripts/controllers/main.js
+++ app/scripts/controllers/main.js
@@ -1,10 +1,13 @@
 'use strict';
 
 angular.module('orderMasterApp')
-  .controller('MainCtrl', function ($scope) {
+  .controller('MainCtrl', function ($scope,$location) {
     $scope.awesomeThings = [
       'HTML5 Boilerplate',
       'AngularJS',
       'Karma'
     ];
+    $scope.help_order = function(){
+        $location.path('/order_details')
+    }
   });
