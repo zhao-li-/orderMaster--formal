48f0772ec2db7762ef1bbcf1b8a9b7396d442452
zhao-li- <1216186168@qq.com>
2014-05-19 10:01:46 +0800

    background to color

--- app/scripts/application.js
+++ app/scripts/application.js
@@ -2,9 +2,9 @@
 
 var myModule = angular.module('orderMasterApp')
 
-myModule.filter('background1', function () {
+myModule.filter('color1', function () {
     return function (food_price) {
         var color = (food_price>=12) ? 'red':'';
-        return 'background : ' + color;
+        return 'color : ' + color;
     };
 });
